7810951ed582095cbce14fa9496a59df2282b720
Ryan Florence <rpflorence@gmail.com>
2014-08-06 23:05:48 -0600

    update to react-router 0.5.x

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,11 +1,15 @@
 /** @jsx React.DOM */
-var Route = require('react-router').Route;
+var Router = require('react-router');
+var Routes = Router.Routes
+var Route = Router.Route;
 
 module.exports = (
-  <Route location="history" handler={require('../components/app')}>
-    <Route name="foo" handler={require('../components/foo')} />
-    <Route name="bar" path="/what/evz" handler={require('../components/bar')} />
-    <Route name="index" path="/" handler={require('../components/index')} />
-  </Route>
+  <Routes>
+    <Route location="history" handler={require('../components/app')}>
+      <Route name="foo" handler={require('../components/foo')} />
+      <Route name="bar" path="/what/evz" handler={require('../components/bar')} />
+      <Route name="index" path="/" handler={require('../components/index')} />
+    </Route>
+  </Routes>
 );
 
