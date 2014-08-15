7bd5a3192ef50e2ac69d33e38080924031fbb62c
Rick Harrison <rick.harrison@me.com>
2014-08-14 17:16:10 -0700

    Moves location to Routes declaration

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,8 +4,8 @@ var Routes = Router.Routes
 var Route = Router.Route;
 
 module.exports = (
-  <Routes>
-    <Route location="history" handler={require('../components/app')}>
+  <Routes location="history">
+    <Route handler={require('../components/app')}>
       <Route name="foo" handler={require('../components/foo')} />
       <Route name="bar" path="/what/evz" handler={require('../components/bar')} />
       <Route name="index" path="/" handler={require('../components/index')} />
