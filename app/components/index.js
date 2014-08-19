1e7103682d9984a478b86821393f322b93763644
Philip Kobernik <ptk921@gmail.com>
2014-08-18 21:14:54 -0700

    search field input updates component state 'searchText'

--- app/components/index.js
+++ app/components/index.js
@@ -3,10 +3,35 @@
 var React = require('react');
 
 var Index = module.exports = React.createClass({
+  getInitialState: function() {
+    return({
+      searchText: '',
+    });
+  },
+
+  onChangeSearchText: function() {
+    var text = this.refs.searchField.getDOMNode().value;
+    this.setState({
+      searchText: text
+    });
+    if(text.length > 3){
+      console.log('triggering search');
+      //this.triggerSearch();
+    }
+  },
+
   render: function() {
     return (
       <div>
-        <h2>Index</h2>
+        <h2>Cannabis Strain Instant Search</h2>
+        <input
+         type='search'
+         classname='input-lg'
+         ref='searchField'
+         value={this.state.searchText}
+         onChange={this.onChangeSearchText}
+         >
+        </input>
       </div>
     );
   }
